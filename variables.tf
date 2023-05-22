9660971d5cd907df201aa81f1dc228c2e48cb2fb
aashishgautam <gautamaashish41@gmail.com>
2023-05-22 15:06:11 +0545

    Git reset done

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "aws_access_key" {
     type = string
-    value = "ASIA52BEGI3BJFSFE6FV"
+    default = "ASIA52BEGI3BJFSFE6FV"
   
 }
 
 variable "aws_secret_access_key" {
     type = string
-    value = "kKBQZJj/iXqYnc9aZMakH3aYB5QlO/KjvxT7myyy"
+    default = "kKBQZJj/iXqYnc9aZMakH3aYB5QlO/KjvxT7myyy"
   
 }
 
 variable "aws_session-token" {
     type = string
-    value = "IQoJb3JpZ2luX2VjEJD//////////wEaCXVzLWVhc3QtMiJGMEQCIG5fq2tIzJbdk5/Ln42oQrdLWU7qm11WVneo06U4XOXBAiAsdnDfWt5b/eIl18Hcrv8TUILBHDdf9ordayhIZjKkUCquAwi5//////////8BEAEaDDk0OTI2MzY4MTIxOCIMZ+tg5saqhvTicX4PKoID9M6dxwcK9VgGMO9AH2gBKsogi/vma9qQdv17/4/1KxMpgVYWr6rNwTRSoJlkHFWvshmmBdtfSvVRjZuy7XbGQ2Hz8Ip0haot4M5IGTf1ItJIVPRYkA2jpXC1wWCWVl5dNqyf6QBlzOTSz/4hKBZWPCMu0kwq81m1kKdLPyRWpo6NPztrmIRR8a3mq3N/F63mpmSnyy80TNgYSJnzT4ksw5oMgCfvVuqz4QdbAhS05J+sjuz3F2TPsFGNtOWL0LTHC5HJkpY8T+0qRzGTBv8oVkHLB1DkKxWKi9rEu5nppbzj+oFwfwUXbkzmkbuJIx+n2NUEkGFSs0GLB633uNPQqvf/8GiPl5x5GzxOokLIP9UUuBLOKMp5XqqUihaRXcTfb4AGx4PPY0ZNXFgY8zPEq+edvoTlNbMyzZVxnKE/IZJOeDz8CHxkjDKJOCWIoLxnsA1XEHrU37UPy+pBlMqHdF36Q+dSJ5NzxxBT29xYxcNTggdtiyjDBVk7a/K3Z0JxR+MwosmsowY6pwHbWiMQnnF/bLiS/ZEDWMokUC/O59tE5JR61kwa+eG5TRe9tDDm+zzbeD0fprKRax7N1EakHhLuTOacNmbvGgcFKHP1N94Ce0gtn7FRgEejZNJRoZC1k2OX3uGdJgubOqKtgOzLbuiMrlQ2oGf+i/Dx7FhCbLdpguUy++prwBGv1Uj2U+qBWr/p4+OKqZD1Ksop3NPZ7TadOFYEzUL9oUScWtr62iZLAw=="
+    default = "IQoJb3JpZ2luX2VjEJD//////////wEaCXVzLWVhc3QtMiJGMEQCIG5fq2tIzJbdk5/Ln42oQrdLWU7qm11WVneo06U4XOXBAiAsdnDfWt5b/eIl18Hcrv8TUILBHDdf9ordayhIZjKkUCquAwi5//////////8BEAEaDDk0OTI2MzY4MTIxOCIMZ+tg5saqhvTicX4PKoID9M6dxwcK9VgGMO9AH2gBKsogi/vma9qQdv17/4/1KxMpgVYWr6rNwTRSoJlkHFWvshmmBdtfSvVRjZuy7XbGQ2Hz8Ip0haot4M5IGTf1ItJIVPRYkA2jpXC1wWCWVl5dNqyf6QBlzOTSz/4hKBZWPCMu0kwq81m1kKdLPyRWpo6NPztrmIRR8a3mq3N/F63mpmSnyy80TNgYSJnzT4ksw5oMgCfvVuqz4QdbAhS05J+sjuz3F2TPsFGNtOWL0LTHC5HJkpY8T+0qRzGTBv8oVkHLB1DkKxWKi9rEu5nppbzj+oFwfwUXbkzmkbuJIx+n2NUEkGFSs0GLB633uNPQqvf/8GiPl5x5GzxOokLIP9UUuBLOKMp5XqqUihaRXcTfb4AGx4PPY0ZNXFgY8zPEq+edvoTlNbMyzZVxnKE/IZJOeDz8CHxkjDKJOCWIoLxnsA1XEHrU37UPy+pBlMqHdF36Q+dSJ5NzxxBT29xYxcNTggdtiyjDBVk7a/K3Z0JxR+MwosmsowY6pwHbWiMQnnF/bLiS/ZEDWMokUC/O59tE5JR61kwa+eG5TRe9tDDm+zzbeD0fprKRax7N1EakHhLuTOacNmbvGgcFKHP1N94Ce0gtn7FRgEejZNJRoZC1k2OX3uGdJgubOqKtgOzLbuiMrlQ2oGf+i/Dx7FhCbLdpguUy++prwBGv1Uj2U+qBWr/p4+OKqZD1Ksop3NPZ7TadOFYEzUL9oUScWtr62iZLAw=="
   
 }
